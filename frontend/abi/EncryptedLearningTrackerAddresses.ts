aff143ec513d0e25909b3eceecdea6004c01d581
dopkopmakop <sodbfdjxvcb@gmail.com>
2025-11-10 15:03:47 +0800

    addresses constants added

---
+++ frontend/abi/EncryptedLearningTrackerAddresses.ts
@@ -0,0 +1,15 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+  
+  Run 'npm run genabi' after deploying the contract to generate the actual addresses.
+  This file automatically includes all available deployments. Missing deployments are skipped.
+*/
+export const EncryptedLearningTrackerAddresses = {
+  "11155111": {
+    "address": "0xfD7081dE716971Cc3451c20535bcf9e6b1f5D6c9",
+    "chainId": 11155111,
+    "chainName": "sepolia"
+  }
+};
